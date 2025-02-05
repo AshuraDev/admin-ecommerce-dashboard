1664e814ec3c334b4bc9bf7581bed0d2d69d6026
AshuraDev <akogbetodonald@gmail.com>
2025-02-05 18:42:11 +0100

    feat(button): Add shadcn button like reusable button component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="w-full h-full flex item-center justify-center text-6xl font-bold text-red-400">Hello world</div>;
+  return (
+    <div className="flex flex-col item-center justify-center text-6xl font-bold text-red-400">
+      Hello world
+      <Button>Click me</Button>
+    </div>
+  );
 }
