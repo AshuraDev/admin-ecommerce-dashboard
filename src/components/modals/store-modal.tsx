713c2c0b183a730b2e23cca48d7300634b915a97
AshuraDev <akogbetodonald@gmail.com>
2025-02-05 23:28:03 +0100

    feat(StoreModal):Add storeModal form with zod validation

--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { formSchema } from "@/schema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export const StoreModal = () => {
+  //
   const { isOpen, onClose } = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  //
   return (
     <Modal
       title="Créer une boutique"
@@ -12,7 +40,33 @@ export const StoreModal = () => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      Futur store form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nom</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Ma Boutique" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant={"destructive"} onClick={onClose}>
+                  Annuler
+                </Button>
+                <Button type="submit">Continuer</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
---
+++ src/schema/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const formSchema = z.object({
+  name: z.string().min(2,{message:"Le nom doit contenir au moins deux caractères"}),
+});
+
+
+// const { isSubmitting, isValid } = form.formState;
