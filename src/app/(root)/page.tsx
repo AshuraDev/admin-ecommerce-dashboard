1fd1bf71bdb520b17bdebb37a6758e3aadf05f87
AshuraDev <akogbetodonald@gmail.com>
2025-02-05 21:06:04 +0100

    feat(modal): Add a reusable modal component base on shadcn Dialog

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,23 @@
-import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+"use client"
+
+import { Modal } from "@/components/ui/modal";
+
+export default function SetupPage() {
   return (
     <div className="p-4">
       <p>Ceci est une route protéger</p>
-      <UserButton />
+      <Modal
+        title="Test title"
+        description="Description test"
+        isOpen
+        onClose={() => {}}
+      >
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis,
+        quisquam minus corporis cumque, delectus nemo a sit provident earum rem,
+        illo voluptatem doloremque dolor aperiam et magnam illum sequi
+        recusandae?
+      </Modal>
     </div>
   );
 }
