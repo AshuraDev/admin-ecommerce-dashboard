eddae34fa6dd70ee216b49d3a57db9455f0bab9c
AshuraDev <akogbetodonald@gmail.com>
2025-02-05 19:46:32 +0100

    feat(Auth): Setup clerk authentification

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex flex-col item-center justify-center text-6xl font-bold text-red-400">
-      Hello world
-      <Button>Click me</Button>
+    <div className="p-4">
+      <p>Ceci est une route protéger</p>
+      <UserButton />
     </div>
   );
 }
