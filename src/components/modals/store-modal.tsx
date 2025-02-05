2ec2a503694b1651b426a90847f2f687cbc85ef5
AshuraDev <akogbetodonald@gmail.com>
2025-02-05 21:56:34 +0100

    feat(modal): Add Zustand for state management, create a hook to open/close the modal, and add a reusable store modal

---
+++ src/components/modals/store-modal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "../ui/modal";
+
+export const StoreModal = () => {
+  const { isOpen, onClose } = useStoreModal();
+  return (
+    <Modal
+      title="Créer une boutique"
+      description="Ajouter une nouvelle boutique pour gérer les produits et les catégories"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      Futur store form
+    </Modal>
+  );
+};
