72c225b808d7c230ebeef3976596eb3bfc9abd98
AshuraDev <akogbetodonald@gmail.com>
2025-02-13 13:58:37 +0100

    Add dashbord overview

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,28 +1,80 @@
-import prismadb from "@/lib/prismadb";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import React from "react";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { Overview } from "@/components/dashboard/overview";
+import { Heading } from "@/components/heading";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { formatPrice } from "@/lib/price-format";
 
-interface DashboardPageProps {
-  params: { storeId: string };
-}
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
-const DashboardPage = async ({ params }: DashboardPageProps) => {
-  const { userId } = await auth();
+const DashboardPage = async ({ params }: { params: Promise<{ storeId: string }> }) => {
   const { storeId } = await params;
 
-  if (!userId) {
-    redirect("/sign-in");
-  }
+  const totalRevenue = await getTotalRevenue(storeId);
+  const salesCount = await getSalesCount(storeId);
+  const stockCount = await getStockCount(storeId);
+  const graphRevenue = await getGraphRevenue(storeId);
 
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: storeId,
-      userId,
-    },
-  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 p-8 pt-6 space-y-4">
+        <Heading title="Dashboard" description="Aperçu de votre boutique" />
+        <Separator />
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+              <CardTitle className="text-sm font-medium">
+              Chiffre d&apos;affaires total
+              </CardTitle>
+              <DollarSign className="w-4 h-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatPrice(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+              <CardTitle className="text-sm font-medium">
+                Ventes
+              </CardTitle>
+              <CreditCard className="w-4 h-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                +{salesCount}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+              <CardTitle className="text-sm font-medium">
+               Stock
+              </CardTitle>
+              <Package className="w-4 h-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {stockCount}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
+}
 
-  return <div>Le nom de votre boutique est: {store?.name}</div>;
-};
-
-export default DashboardPage;
+export default DashboardPage
