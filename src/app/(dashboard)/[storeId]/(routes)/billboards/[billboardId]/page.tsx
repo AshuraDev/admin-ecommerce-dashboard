c2d856c40cb7b66d3747e09fd5201a82a4ba22a6
AshuraDev <akogbetodonald@gmail.com>
2025-02-13 14:43:18 +0100

    Fix Type error does not satisfy the constraint 'PageProps'

--- src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "@/components/dashboard/billboards/billboard-form";
 
-const BillboardPage = async ({
-  params,
-}: {
+interface BillboardPageProps {
   params: { billboardId: string };
-}) => {
+}
+
+const BillboardPage = async ({ params }: BillboardPageProps) => {
   const { billboardId } = await params;
 
   const billboard = await prismadb.billboard.findUnique({
@@ -19,7 +19,7 @@ const BillboardPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-       <BillboardForm initialData={billboard}/>
+        <BillboardForm initialData={billboard} />
       </div>
     </div>
   );
